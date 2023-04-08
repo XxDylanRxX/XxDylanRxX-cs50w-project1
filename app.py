59003de80e904822ee9d3c3311a518f7a4b1d088
Dylan Rizo <you@example.com>
2023-04-08 03:38:34 -0600

    creacion y llamado de api

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
 
-from flask import Flask, session, render_template ,request , redirect, url_for
+from flask import Flask, session, render_template ,request , redirect, url_for, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.security import check_password_hash, generate_password_hash
 from dotenv import load_dotenv
 from datetime import datetime
+import requests
+
 app = Flask(__name__)
 
 # Check for environment variable
@@ -109,27 +111,62 @@ def PaginaLibro(libro_isbn, libro_id):
     query = text("SELECT * FROM books WHERE  isbn= :libro_isbn ")
     resultado = db.execute(query, {"libro_isbn": libro_isbn}).fetchall()
     print(resultado)
+    isbn= libro_isbn
+    response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn).json()
+    ratings_count = response["items"][0]["volumeInfo"]["ratingsCount"]
+    average_rating = response["items"][0]["volumeInfo"]["averageRating"]
+    print(ratings_count)
+    print(average_rating)
+   
     if request.method == 'POST':
         reseña = request.form.get('reseña') 
         rating = request.form.get('rating')
         fecha_reseña = datetime.now() 
         id_user = session['user_id'][0]
+
         if reseña is not None:
-            query = text("SELECT * FROM reseñas WHERE id_user = :id_user AND id_books = id_books")
+            query = text("SELECT * FROM reseñas WHERE id_books = id_books AND id_user = :id_user ")
             resultado2 = db.execute(query, {"id_user": id_user, "id_books": libro_id}).fetchall()
             print(len(resultado2))
-            if len(resultado2) >= 2 :
-                return "Ya has publicado una reseña"
+            if len(resultado2) >= 1 :
+                return "Ya has publicado una reseña para este libro"
             else:
-                query = text("INSERT INTO reseñas (id_books, id_user,reseña, rating, fecha_reseña  ) VALUES (:id_books, :id_user, :reseña, :rating, :fecha_reseña )")
+                query = text("INSERT INTO reseñas (id_books, id_user,reseña, rating, fecha_reseña) VALUES (:id_books, :id_user, :reseña, :rating, :fecha_reseña )")
                 resultado3= db.execute(query,{"id_books": libro_id, "id_user":id_user,"reseña":reseña, "rating":rating, "fecha_reseña": fecha_reseña })
                 db.commit()
-                query2 = text("SELECT * FROM reseñas WHERE id_user = :id_user")
-                resultado2 = db.execute(query2, {"id_user": id_user}).fetchall()
-                return render_template("infolibro.html", resultado3 = resultado3)
+                query2 = text("SELECT * FROM reseñas WHERE id_books = id_books AND id_user = :id_user ")
+                resultado2 = db.execute(query2, {"id_user": id_user, "id_books": libro_id}).fetchall()
+                print(response)
+                print(resultado3)
+                
+                return render_template("reseñas.html", resultado2 = resultado2)
         else:
             return "agrega una reseña"
     return render_template("infolibro.html", resultado = resultado)
+@app.route("/apiBooks/<string:libro_isbn>/<string:libro_id>")
+def books_api(libro_isbn,libro_id ):
+    """Return details about a single flight."""
+
+    # Make sure flight exists.
+
+    if libro_isbn is None:
+        return jsonify({"error": "Invalid ISBN"}), 422
+
+    # Get all passengers.
+    query = text("SELECT * FROM books WHERE  isbn= :libro_isbn ")
+    resultado = db.execute(query, {"libro_isbn": libro_isbn}).fetchall()
+    for result in resultado:
+        namebook = result.title
+        authorbook = result.author
+        yearbook = result.year
+
+
+    return jsonify({
+            "title": namebook,
+            "author": authorbook,
+            "year": yearbook,
+            "isbn": libro_isbn
+        })
 
 
 if __name__ == '__main__':
